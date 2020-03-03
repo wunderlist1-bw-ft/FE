aa120a2598307b547049f9ff78af766a4b2781e2
Amber Mohler <ambermohler@iMac.local>
2020-03-02 19:22:40 -0500

    trying to fix merge conflicts

--- wunderlist/src/App.js
+++ wunderlist/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
-import PrivateRoute from './components/PrivateRoute'
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import Dashboard from './components/Dashboard'
+import { Route } from "react-router-dom";
+import LoginSignup from "./components/onboarding/LoginSignup";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="login-signup">
-        <h1>Welcome!</h1>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-
-        <Route path="/login" component={Login} />
-        <PrivateRoute path='/dashboard' component={Dashboard} />
-        <Route path="/signup" component={Signup} />
-      </div>
+      <Route path="/">
+        <LoginSignup />
+      </Route>
     </div>
   );
 }
